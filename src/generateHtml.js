dde467ebafce26b3d50d1fffb686819f49205929
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2022-01-09 06:40:44 -0500

    Add HTML & CSS
    - add html template
    - add css file
    - stub generateHtml file
    - add image icons

---
+++ src/generateHtml.js
@@ -0,0 +1,8 @@
+const Manager = require('./class/Manager');
+const Engineer = require('./class/Engineer');
+const Intern = require('./class/Intern');
+
+
+function renderCards() {
+  return Card;
+}
